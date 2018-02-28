aa8ee63b80a0dde367f1d8ab329bc2b2df20af98
5adiyah <sadiali@pdx.edu>
2018-02-27 18:15:43 -0800

    It will take in a user input, generate random computer play and print the winner

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
-alert("hello");
+//begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round
+let computerChoice;
+let playerChoice;
+
+let playerSelection = () => {
+  let playerInput = prompt('Pick Rock Paper or Scissors');
+  playerChoice = playerInput;
+  computerPlay();
+}
+
+let computerPlay = () => {
+  let random = Math.floor(Math.random() * 3);
+  if(random == 0){
+    computerChoice = 'rock';
+  } else if(random == 1){
+    computerChoice = 'paper';
+  } else {
+    computerChoice = 'scissors';
+  }
+  singleRound(computerChoice, playerChoice);
+}
+
+
+let singleRound = (computerChoice, playerChoice) => {
+  if((playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors' )||
+     (playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock') ||
+     (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'paper')){
+       console.log(`The computer chose ${computerChoice} you win!`)
+     } else{
+       console.log(`The computer chose ${computerChoice} you lost!`)
+     }
+}
+
+playerSelection();
